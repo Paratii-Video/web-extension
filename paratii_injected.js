f95a2e6f08c926f0fb4c880bc749698bc96445d9
Yahya <ya7yaz@gmail.com>
2017-10-26 20:50:39 +0200

    clean up

--- paratii_injected.js
+++
@@ -1,9 +0,0 @@
-window.paratiiExtensionFunction = function() {
-	alert('Yay it works!');
-};
-
-$(function() {
-	if($('#paratiiDetectorElement').length) {
-		window.paratiiExtensionFunction();
-	}
-});
